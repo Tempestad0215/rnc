f331b0d5e8ff391efb09c0a6374c1ecd7de12fce
Mario Guzman <marioguzman140@gmail.com>
2026-06-23 17:07:32 -0400

    Guardando en la base de datos

--- app/Imports/RncImport.php
+++ app/Imports/RncImport.php
@@ -6,7 +6,6 @@ use App\Models\RNC;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
-use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
 class RncImport implements ToCollection, WithChunkReading
 {
@@ -16,35 +15,27 @@ class RncImport implements ToCollection, WithChunkReading
      */
     public function collection(Collection $collection): void
     {
-
-        $insertData = [];
         foreach ($collection as $row)
         {
             if(!isset($row[0]) || !isset($row[1])) {
                 continue;
             }
 
-            // Acumulamos los datos limpios en el arreglo
-            $insertData[] = [
-                'rnc'              => trim($row[0]),
-                'razon_social'     => trim($row[1]),
-                'actividad' => isset($row[2]) ? trim($row[2]) : '',
-                'status' => isset($row[5]) ? trim($row[5]) : '',
-                'type' => isset($row[6]) ? trim($row[6]) : '',
-                'created_at'       => now(),
-                'updated_at'       => now(),
-            ];
-
-
+            $rnc = trim($row[0]);
+            $razonSocial = trim($row[1]);
+            $nombreComercial = isset($row[2]) ? trim($row[2]) : '';
+            $status = isset($row[5]) ? trim($row[5]) : '';
+            $type = isset($row[6]) ? trim($row[6]) : '';
 
             // Crear los datos
-            if (!empty($insertData)) {
-                RNC::upsert(
-                    $insertData,
-                    ['rnc'], // Columna única para identificar duplicados
-                    ['razon_social', 'actividad','status','type'] // Columnas a actualizar si ya existe
-                );
-            }
+            RNC::updateOrCreate([
+                'rnc' => $rnc,
+            ],[
+                'razon_social' => $razonSocial,
+                'actividad' => $nombreComercial,
+                'status' => $status,
+                'type' => $type,
+            ]);
         }
     }
 
